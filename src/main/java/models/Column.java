871d4cf9828d549c2a6b48228c96c81957470d55
David Sahni <sahnid@oregonstate.edu>
2017-11-10 11:12:11 -0800

    finished column class

--- src/main/java/models/Column.java
+++ src/main/java/models/Column.java
@@ -2,7 +2,7 @@ package models;
 import java.util.ArrayList;
 
 public class Column {
-    public java.util.List<Card> stack = new ArrayList<Card>();
+    private java.util.List<Card> stack = new ArrayList<Card>();
 
     public Card getTopCard(){
             return this.stack.get(stack.size()-1);
@@ -16,5 +16,16 @@ public class Column {
         Card c = this.getTopCard();
         this.removeTopCard();
         return c;
+    }
+
+    public void addCard(Card c){
+        stack.add(c);
+    }
+
+    public boolean hadCards(){
+        if (this.stack.size() > 0){
+            return true;
+        }
+        return false;
     }
 }
