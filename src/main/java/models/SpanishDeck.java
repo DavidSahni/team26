4d79555eae5782a588390175748f6bc9c7257255
David Sahni <sahnid@oregonstate.edu>
2017-12-01 19:21:45 -0800

    Added backend routing for sending the deck key on game creation

--- src/main/java/models/SpanishDeck.java
+++ src/main/java/models/SpanishDeck.java
@@ -5,7 +5,13 @@ public class SpanishDeck extends Deck {
 
 
    public void init() {
-        //ian this is you
+        for(int i = 2; i < 16; i++){
+            deck.add(Card.createCard(i, Suit.Clubs));
+            deck.add(Card.createCard(i, Suit.Hearts));
+            deck.add(Card.createCard(i, Suit.Diamonds));
+            deck.add(Card.createCard(i, Suit.Spades));
+        }
+        shuffle();
     }
 
 
