92d76a2cf8fbc4f29c05a1d02571cb305b727309
David Sahni <sahnid@oregonstate.edu>
2017-12-02 15:28:46 -0800

    was just for testing, not actual spanish deck implementation

--- src/main/java/models/SpanishDeck.java
+++ src/main/java/models/SpanishDeck.java
@@ -5,13 +5,7 @@ public class SpanishDeck extends Deck {
 
 
    public void init() {
-        for(int i = 2; i < 16; i++){
-            deck.add(Card.createCard(i, Suit.Clubs));
-            deck.add(Card.createCard(i, Suit.Hearts));
-            deck.add(Card.createCard(i, Suit.Diamonds));
-            deck.add(Card.createCard(i, Suit.Spades));
-        }
-        shuffle();
+        //ian this is you
     }
 
 
