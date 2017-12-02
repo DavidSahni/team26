e9fa3043a34db4802554809268e2dbf9b66dbcd5
Ian Abbene <abbenei@oregonstate.edu>
2017-12-02 15:26:24 -0800

    Added Espadas, Copas, Oros, Bastos as suits to the Game

--- src/main/java/models/SpanishDeck.java
+++ src/main/java/models/SpanishDeck.java
@@ -5,8 +5,17 @@ public class SpanishDeck extends Deck {
 
 
    public void init() {
-        //ian this is you
-    }
+       
+       for(int i = 1; i < 13; i++){
+       		deck.add(Card.createCard(i, Suit.Bastos));
+       		deck.add(Card.createCard(i, Suit.Oros));
+       		deck.add(Card.createCard(i, Suit.Copas));
+       		deck.add(Card.createCard(i, Suit.Espadas));
+       }
+       deck.add(Card.createCard(14, Suit.Comodines));
+       deck.add(Card.createCard(14, Suit.Comodines));
+       shuffle();
+;    }
 
 
 
