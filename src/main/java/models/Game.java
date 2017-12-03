9b9904b3f4f5d9df1977d0827d808988acd8cf34
David Sahni <sahnid@oregonstate.edu>
2017-12-02 18:38:35 -0800

    added a few more test methods, fixed some bugs

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -24,10 +24,9 @@ public class Game {
     public boolean userWon = false;
 
     public Game(){
-        cols.add(new Column(1)); //colms.add(new Column())
-        cols.add(new Column(2));
-        cols.add(new Column(3));
-        cols.add(new Column(3));
+        for(int i = 1; i < 5; i++) {
+            cols.add(new Column(i)); //colms.add(new Column())
+        }
     }
 
     public void init(int key){
@@ -38,9 +37,6 @@ public class Game {
         }else{
             deck = new SpanishDeck();
         }
-        for(int i = 1; i < 5; i++) {
-            cols.add(new Column(i)); //colms.add(new Column())
-        }
         deck.init();
     }
 
@@ -48,7 +44,9 @@ public class Game {
 
     public void dealFour() {
         for(int i = 0; i < 4; i++){
-            cols.get(i).cards.add(deck.dealTop()); //colms.get(i).addCard(deck.dealTop());
+            if (deck.returnDeckSize() > 0) {
+                cols.get(i).cards.add(deck.dealTop()); //colms.get(i).addCard(deck.dealTop());
+            }
         }
     }
 
--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -45,23 +45,54 @@ public class testGame {
     @Test
     public void testRemove(){
         g = new Game();
-        g.init(0);
+        g.init(1);
         int rem = g.removeCount;
         g.remove(0);
         assertEquals(g.removeCount, rem); //no cards should have been removed
-        g.dealFour();
+
+        g.cols.get(0).cards.add(new Card(2, Suit.Bastos));
+        g.cols.get(1).cards.add(new Card(12, Suit.Bastos));
+
+        g.remove(1);
+        assertEquals(g.removeCount, rem); //no cards should have been removed
 
         g.remove(0);
         assertEquals(g.removeCount, rem+1);
         assertEquals(g.cols.get(0).cards.size(), 0);
 
+        g.cols.get(2).cards.add(new Card(14, Suit.Comodines));
+        rem = g.removeCount;
+
+        g.remove(1);
+        assertEquals(g.removeCount, rem+2);
+        assertEquals(g.cols.get(1).cards.size(), 0);
+        assertEquals(g.cols.get(2).cards.size(), 0);
+
         g.remove(5);
-        assertEquals(g.removeCount, rem+1); //shouldn't have increased no column 5
+        assertEquals(g.removeCount, rem+2); //shouldn't have increased no column 5
 
     }
 
+    @Test
     public void testMove(){
         g = new Game();
+        g.init(1);
         g.cols.get(0).cards.add(new Card(0, Suit.Comodines));
+        g.move(0, 1);
+        assertEquals(g.validMove, false);
+        assertEquals(g.cols.get(0).cards.size(), 1);
+        assertEquals(g.cols.get(1).cards.size(), 0);
+
+        g.cols.get(1).cards.add(new Card(12, Suit.Bastos));
+        g.move(1, 0);
+        assertEquals(g.validMove, false);
+        assertEquals(g.cols.get(0).cards.size(), 1);
+        assertEquals(g.cols.get(1).cards.size(), 1);
+
+        g.move(1,2);
+        assertEquals(g.validMove, true);
+        assertEquals(g.cols.get(1).cards.size(), 0);
+        assertEquals(g.cols.get(2).cards.size(), 1);
+        assertEquals(g.cols.get(2).cards.get(0).value, 12);
     }
 }
